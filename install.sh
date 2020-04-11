ec8dc1c11cc6f95ed427b452fce08f4a83bf23ff
BrainlessDylan <61665165+BrainlessDylan@users.noreply.github.com>
2020-04-11 21:37:49 +0100

    Update install.sh
    edited for the stuff i need to get my work done

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
 
 # Initial Software
 
+
 sudo apt update
 
-sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack \
-net-tools htop grsync lame asunder soundconverter brasero git mc shutter \
+sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack \ htop grsync lame asunder soundconverter brasero git mc shutter \
 openssh-server sshfs gedit-plugin-text-size obs kdenlive nano mpv vlc \
-remmina qt5-style-plugins spell synaptic okular audacity terminator -yy
+qt5-style-plugins spell synaptic okular terminator -yy
 
 # Add me to any groups I might need to be a part of:
 
@@ -34,11 +34,19 @@ sudo gdebi -n google-chrome-stable_current_amd64.deb
 popd
 rm -rf /tmp/gc-install-tmp
 
+# Install vscodium IDE
+mkdir /tmp/code-install-tmp
+pushd /tmp/code-install-tmp
+wget https://github.com/VSCodium/vscodium/releases/download/1.44.0/codium_1.44.0-1586429109_amd64.deb
+sudo gdebi -n codium_1.44.0-1586429109_.deb
+popd
+rm -rf /tmp/code-install-tmp
+
 ## Multimedia
 sudo apt-get install -y gimp audacious nautilus-dropbox
 
 ## Games
-sudo apt-get install -y steam lutris
+sudo apt-get install -y steam lutris gamemode
 
 # Gotta reboot now:
 sudo apt update && sudo apt upgrade -y
